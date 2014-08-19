befaae5d8a86f5891dcc855f00414ac50fc6a36c
Ismael Cuevas Román <ismael_c_r@hotmail.com>
2014-08-19 11:21:36 -0500

    dos
    Se le anexa el archivo config.xml

--- js/acciones.js
+++ js/acciones.js
@@ -1,13 +1,12 @@
 //JavaScript
 $(document).ready(function(e){	
 document.addEventListener("deviceready", function(){
-	$('#beep').tap(function(){
-		navigator.notification.beep(1);		
+	$('#btnBeep').tap(function(){
+		navigator.notification.beep(2);		
+		alert("Hola");
 		});
-	$('#vibrar').tap(function(){
+	$('#btnVibrar').tap(function(){
 		navigator.notification.vibrate(1000);		
-		});
-		
-	},false);	
-	
+		});		
+	},false);		
 });//ready
